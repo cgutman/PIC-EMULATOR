f8e9a014625364a0197e4ee84559f4f50e79fd7b
Cameron Gutman <cgutman@aicom-mac.student.cwru.edu>
2012-11-13 04:58:03 -0500

    Initial Commit

---
+++ PIC16F84A Emulator/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  PIC16F84A Emulator
+//
+//  Created by Cameron Gutman on 11/13/12.
+//  Copyright (c) 2012 Cameron Gutman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
